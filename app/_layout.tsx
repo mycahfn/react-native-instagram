c7b0fd808b32f4b26bdb3c8f9c25384393931852
mikael <mikaelfernandezj@gmail.com>
2025-12-14 14:32:59 -0400

    added status bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,22 +1,34 @@
 import ThemeLayout from "@/components/ThemeLayout";
-import { Stack } from "expo-router";
+import { useThemeColor } from '@/hooks/use-theme-color';
 
+import { Stack } from "expo-router";
+import { StatusBar } from 'expo-status-bar';
 
 export const unstable_settings = {
   anchor: '(tabs)',
 };
 
 export default function RootLayout() {
+
+  const backgroundColor = useThemeColor({}, 'background');
+  
   return (
     <ThemeLayout>
        <Stack.Protected guard={true}>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen 
+            name="(tabs)" 
+            options={{ 
+              headerShown: false,
+              contentStyle: { backgroundColor }
+            }} 
+            />
           
         </Stack.Protected>
         <Stack.Protected guard={false}>
           <Stack.Screen name="auth" options={{ headerShown: false }} />
           <Stack.Screen name="notifications" options={{ headerShown: false }} />
         </Stack.Protected>
+        <StatusBar style="auto" />
     </ThemeLayout>
   );
 }
